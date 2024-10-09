5b7f28f2f7605329b9b1a8a7a1c1958ad835f68e
rompahel <rompahel77@gmail.com>
2024-10-10 00:33:07 +0700

    gg dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -84,7 +84,7 @@ st.markdown("""
 - This indicates a potential seasonal pattern where air quality improves during the summer and worsens in the spring.
 """)
 
-st.subheader('Pertanyaan 1')
+st.subheader('Visualisasi Pertanyaan 1')
 
 # Correlation Heatmap Visualization
 st.subheader('Correlation Analysis')
@@ -103,7 +103,7 @@ Pertanyaan 1:**
 - This supports the idea that wind plays a role in dispersing particulate matter, contributing to better air quality.
 """)
 
-st.subheader('Pertanyaan 1')
+st.subheader('Visualisasi Pertanyaan 2')
 
 # Monthly Average PM10 Levels Over Time
 st.subheader('Seasonal Trends in PM10 Levels')
